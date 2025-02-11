a01d50df320477ba486709c4686a7e5ac6e69eb7
FilippaBo <filippa@bonorden.se>
2025-02-11 16:03:12 +0100

    klar med completed program

--- lab1/topmost.py
+++ lab1/topmost.py
@@ -3,31 +3,34 @@ import sys
 
 
 
-
-inp_file = open(sys.argv[1]) # hämtar stopord från fil
-inp_file2 = open(sys.argv[2]) # hämtar textrader från artiklar
-article = int(sys.argv[3]) #gör om 20 från str till int
-#inp_file = open(sys.argv[1], encoding="utf-8")   för windows
-
-stopwordlist=[]
-textrows=[]
-
-for line in inp_file:
-    line=line.strip()
-    stopwordlist.append(line)
-
-for line in inp_file2:
-    textrows.append(line)
-
-inp_file.close()
-inp_file2.close()
-
-
 def main():
+
+    inp_file = open(sys.argv[1]) # hämtar stopord från fil
+    inp_file2 = open(sys.argv[2]) # hämtar textrader från artiklar
+    article = int(sys.argv[3]) #gör om 20 från str till int
+    #inp_file = open(sys.argv[1], encoding="utf-8")   för windows
+
+    stopwordlist=[]
+    textrows=[]
+
+    for line in inp_file:
+        line=line.strip()
+        stopwordlist.append(line)
+
+    for line in inp_file2:
+        textrows.append(line)
+
+    inp_file.close()
+    inp_file2.close()
+
+
     words=wordfreq.tokenize(textrows)
     count=wordfreq.countWords(words, stopwordlist)
     wordfreq.printTopMost(count,article)
+    
+
 
 main()
 
+#obs skriv python3 topmost.py eng_stopwords.txt examples/article1.txt 20 i terminalen varje gång
 
